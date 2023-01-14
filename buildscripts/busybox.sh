b5b96645ecdf713f72182f343d7f1b9e4f89af29
oerg866 <oerg866@googlemail.com>
2023-01-14 18:50:53 +0100

    Fix busybox and dosfstools build scripts

--- buildscripts/busybox.sh
+++ buildscripts/busybox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-make -j16 ARCH=x86 CROSS_COMPILE=$BASE/i486-linux-musl-cross/bin/i486-linux-musl-
-make -j16 ARCH=x86 CROSS_COMPILE=$BASE/i486-linux-musl-cross/bin/i486-linux-musl- install
+make -j8 ARCH=x86 CROSS_COMPILE=../i486-linux-musl-cross/bin/i486-linux-musl-
+make -j8 ARCH=x86 CROSS_COMPILE=../i486-linux-musl-cross/bin/i486-linux-musl- install
