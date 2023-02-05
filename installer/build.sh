b728cc99b755e2fb6193a071e79aac821aaa4609
oerg866 <oerg866@googlemail.com>
2023-02-05 06:22:36 +0100

    installer: Compile with optimizations

--- installer/build.sh
+++ installer/build.sh
@@ -2,4 +2,4 @@
 
 CC=$PWD/../i486-linux-musl-cross/bin/i486-linux-musl-gcc
 
-$CC --static -Wall -pthread main.c ringbuf.c util.c disk.c install_ui.c install.c -lpthread -ldialog -lncurses -ltinfo -olunmercy
+$CC -O2 --static -Wall -pthread main.c ringbuf.c util.c disk.c install_ui.c install.c -lpthread -ldialog -lncurses -ltinfo -olunmercy
