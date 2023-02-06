0d7897bb27050a6b3b00c41e5d59502d1dc69207
oerg866 <oerg866@googlemail.com>
2023-02-06 09:05:10 +0100

    (Attempt to) Fix USB boot not finding install media.
    USB sometimes is not up and enumerated before the init script runs.

--- supplement/findcd.sh
+++ supplement/findcd.sh
@@ -31,33 +31,35 @@ mkdir -p $CD
 cd /dev
 
 while :
-do 
-	if tryMountPattern sr iso9660; then break; fi;		# CD-ROM
-	if tryMountPattern cd iso9660; then break; fi;	    # CD-ROM (alternate)
-	if tryMountPattern ide iso9660; then break; fi;		# CD-ROM (alternate)
-	if tryMountPattern sd iso9660; then break; fi;		# Flash drives can be in ISO 
-	if tryMountPattern hd iso9660; then break; fi;		# Flash drives can be in ISO 
-	if tryMountPattern sd vfat; then break; fi;			# Flash drive or hard drive in FAT32
-	if tryMountPattern hd vfat; then break; fi;			# Flash drive or hard drive in FAT32
-	if tryMountPattern nv vfat; then break; fi;			# NVME (lol?)
-	break
+do
+	if tryMountPattern sr iso9660; then break; fi;	# CD-ROM
+	if tryMountPattern cd iso9660; then break; fi;	# CD-ROM (alternate)
+	if tryMountPattern ide iso9660; then break; fi;	# CD-ROM (alternate)
+	if tryMountPattern sd iso9660; then break; fi;	# Flash drives can be in ISO
+	if tryMountPattern hd iso9660; then break; fi;	# Flash drives can be in ISO
+	if tryMountPattern sd vfat; then break; fi;	# Flash drive or hard drive in FAT32
+	if tryMountPattern hd vfat; then break; fi;	# Flash drive or hard drive in FAT32
+	if tryMountPattern nv vfat; then break; fi;	# NVME (lol?)
+	echo "Cannot find install media (yet). Retrying in 2 seconds."
+	echo "Hit CTRL+C to cancel and drop to a shell."
+	sleep 2
 done
 
 # back to root
 cd /
 
 if test -z "$DEV"; then
-	echo Install media / CD-ROM Not found!
+	echo "Install media / CD-ROM Not found!"
 	return 127
 else
-	echo Install media / CD-ROM found and mounted at $CD
+	echo "Install media / CD-ROM found and mounted at $CD"
 	# Copy some files to memory for faster execution
 	cp $CD/bin/lunmercy /bin
 	cp $CD/bin/cfdisk /bin
 	cp $CD/bin/lsblk /bin
 	cp $CD/bin/mkfs.fat /bin
 	cp $CD/install.txt /
-	
+
 	export CDROM=$CD
 	export CDDEV=$DEV
 	export PATH=$PATH:$CD/bin
