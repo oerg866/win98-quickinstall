931b5f91bcbde79ab2f63d5721662528ad04e3f8
Eric Voirin <oerg866@googlemail.com>
2024-09-06 19:08:38 +0200

    dosflop: Fix mcopy -b param regression on recent distros

--- dosflop/build.sh
+++ dosflop/build.sh
@@ -5,6 +5,6 @@ dd if=/dev/zero of=dosflop.img bs=1440k count=1
 mkfs.fat -F 12 dosflop.img
 dd if=boot/fdboot.bin of=dosflop.img bs=512 count=1 conv=notrunc
 pushd boot
-mcopy -bsv -i "../dosflop.img" * ::
+mcopy -sv -i "../dosflop.img" * ::
 popd
 
