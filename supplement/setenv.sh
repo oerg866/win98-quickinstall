a4e8b463785604ab0f72ee4c7a70295e5ffc0bca
Eric Voirin <oerg866@googlemail.com>
2024-09-08 05:26:27 +0200

    Increase available system memory by changing overcommit_memory and _ratio

--- supplement/setenv.sh
+++ supplement/setenv.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-# Win98SE QuickInstall environment variables
+# Win9x QuickInstall environment setup
 export TERMINFO=/usr/lib/terminfo
 export LANG=C.UTF-8
+
+
+echo 1 >/proc/sys/vm/overcommit_memory
+echo 90 >/proc/sys/vm/overcommit_ratio
