2afc2fb6bfa29d547e25771e5c3a25a5f99469f9
Eric Voirin <oerg866@googlemail.com>
2026-03-07 19:18:00 +0100

    buildscripts: force disable ZLIB when building pciutils

--- buildscripts/pciutils.sh
+++ buildscripts/pciutils.sh
@@ -3,4 +3,4 @@
 export CC=$PWD/../i486-linux-musl-cross/bin/i486-linux-musl-gcc
 export CXX=$PWD/../i486-linux-musl-cross/bin/i486-linux-musl-g++
 
-make -j8 CROSS_COMPILE=$PWD/../i486-linux-musl-cross/bin/i486-linux-musl- LDFLAGS="--static"
+make -j8 ZLIB=no CROSS_COMPILE=$PWD/../i486-linux-musl-cross/bin/i486-linux-musl- LDFLAGS="--static"
