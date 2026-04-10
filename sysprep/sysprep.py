0af2611cfd35e2824a68611ffaa7950e076cc095
Eric Voirin <oerg866@googlemail.com>
2026-04-10 13:46:53 +0200

    sysprep: Fix msdos.exe environment error Fixes #99

--- sysprep/sysprep.py
+++ sysprep/sysprep.py
@@ -179,11 +179,11 @@ def run_regedit(reg_file):
     # regedit is called from *within* msdos.exe.
 
     if platform.system() == 'Windows':
-        subprocess.run([msdos_exe, regedit_exe, '/L:SYSTEM.DAT', '/R:SYSTEM.DAT', reg_file], check=True, stdout=global_stdout)
+        subprocess.run([msdos_exe, '-e', regedit_exe, '/L:SYSTEM.DAT', '/R:SYSTEM.DAT', reg_file], check=True, stdout=global_stdout)
     else:
         reg_file = get_wine_path(reg_file)
         regedit_exe = get_wine_path(regedit_exe)
-        subprocess.run(['wine', msdos_exe, regedit_exe, '/L:SYSTEM.DAT', '/R:SYSTEM.DAT', reg_file], check=True, stdout=global_stdout)
+        subprocess.run(['wine', msdos_exe, '-e', regedit_exe, '/L:SYSTEM.DAT', '/R:SYSTEM.DAT', reg_file], check=True, stdout=global_stdout)
 
 # Copy a file out of a FAT image, does not preserve attributes and datetime
 def image_copy_out(fs: FAT.Dirtable, filename, output):
